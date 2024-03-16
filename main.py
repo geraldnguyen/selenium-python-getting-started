aa31025e4d43059036983e77bf408fbbaeb42afe
Gerald Nguyen <ngthehuy@gmail.com>
2024-03-16 23:02:02 +0800

    reformat tc1

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium import webdriver
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def tc1_get_python_org_title():
+    driver = webdriver.Chrome()
+
+    try:
+        driver.get("https://www.python.org")
+
+        print(driver.title)
+    finally:
+        driver.quit()
+
+# tc1_get_python_org_title()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('UI Automation with Selenium and Python')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
