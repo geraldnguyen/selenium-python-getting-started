2948378f18ee43b1265bb936b8b76be94b1a6a05
Gerald Nguyen <ngthehuy@gmail.com>
2024-03-20 08:38:51 +0800

    unittest

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+class TestPythonOrg(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
 
-def tc1_get_python_org_title():
-    driver = webdriver.Chrome()
+    def tearDown(self):
+        self.driver.quit()
 
-    try:
+    def test_tc1_get_python_org_title(self):
+        driver = self.driver
         driver.get("https://www.python.org")
 
         print(driver.title)
-    finally:
-        driver.quit()
+        self.assertEqual("Welcome to Python.org", driver.title)
 
 
-# tc1_get_python_org_title()
+    def test_tc2_search_getting_started(self):
+        driver = self.driver
 
-
-def tc2_search_getting_started():
-    driver = webdriver.Chrome()
-
-    try:
         driver.get("https://www.python.org")
 
         search_input_field = driver.find_element(By.ID, "id-search-field")
@@ -30,12 +30,12 @@ def tc2_search_getting_started():
         search_go_button.click()
 
         print("current url: " + driver.current_url)
+        self.assertEqual("https://www.python.org/search/?q=getting+started+in+Python&submit=", driver.current_url)
 
         results = driver.find_elements(By.CSS_SELECTOR, ".list-recent-events li")
         print(f"number of results: {len(results)}")
+        self.assertGreater(len(results), 0)
 
-    finally:
-        driver.quit()
+if __name__ == '__main__':
+    unittest.main()
 
-
-tc2_search_getting_started()
